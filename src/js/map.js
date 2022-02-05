8fc8cbd4c85ee3b38a416018af6e01892e904e66
Calvin Yeh <cyeh@mpiwg-berlin.mpg.de>
2022-02-05 13:24:57 +0100

    Refactoring: for reusing, extract the "FloatPanelManagement" Class into the "champ/utilities" project.

--- src/js/map.js
+++ src/js/map.js
@@ -1,9 +1,11 @@
 import { MapLayersController } from "@chmap/leaflet-layers-panel";
 import { PublicMapsController } from "@chmap/public-maps";
 import { DataLayerController } from "@chmap/data-layer";
-import { GeoReferencingController } from '@chmap/geo-referencing';
+import { GeoReferencingController } from "@chmap/geo-referencing";
 
-import { Popover } from '@chmap/utilities/src/bootstrp-wrap';
+import { FloatPanelManagement } from "@chmap/utilities";
+
+import { Popover } from "@chmap/utilities/src/bootstrp-wrap";
 
 // Load Styles
 import '../scss/map.scss';
@@ -102,68 +104,6 @@ function initSidebar(){
     }
 
 }
-
-const FloatPanelManagement = function (){
-
-    const panelMap = {
-        right: [],
-        left: [],
-    };
-
-    function setPanelSeq(panels, panelSeq, newPanel){
-
-        for(const panel of panels){
-            if(panel.id !== newPanel.id) {
-                panelSeq.push(panel);
-            }
-        }
-
-        for(let idx = 0, len = panelSeq.length; idx < len; idx++){
-            panelSeq[idx].style.zIndex = 9999 - idx;
-        }
-
-    }
-
-    function shown(position, panel){
-
-        const panels = panelMap[position];
-
-        const panelSeq = [ panel ];
-
-        setPanelSeq(panels, panelSeq, panel);
-
-        panelMap[position] = panelSeq;
-
-    }
-
-    function hidden(position, panel){
-
-        const panels = panelMap[position];
-
-        const panelSeq = [ ];
-
-        setPanelSeq(panels, panelSeq, panel);
-
-        panelMap[position] = panelSeq;
-
-    }
-
-    function register(controller, eventPrefix, position){
-
-        const shownEventName = `${eventPrefix}.shown`;
-        const hiddenEventName = `${eventPrefix}.hidden`;
-
-        controller.on(shownEventName, (panel) => shown(position, panel));
-
-        controller.on(hiddenEventName, (panel) => hidden(position, panel));
-
-    }
-
-    return {
-        register,
-    }
-
-}();
 
 function cmpEventBinding(){
 
